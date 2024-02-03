6207a7e764e70741b0b812899585c2ddd37f80f2
Aaron Storrer <aaron@storrer.net>
2024-02-03 15:58:02 -0700

    Add bootstrap css styles.
    Add css classes to elements and grid/bootstrap cards.

--- src/App.js
+++ src/App.js
@@ -38,54 +38,116 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" class="container">
+      <div class="container mb-4 mt-4">
         <h1>Kadince Todo App</h1>
-        <div>
-          <form>
-            <input type="text" value={todoInput} onChange={(e) => setTodoInput(e.target.value)} />
-            <button type="submit" onClick={addTodo}>Add</button>
-            <select value={todoStatus} onChange={(e) => setTodoStatus(e.target.value)}>
-              <option value="all">All</option>
-              <option value="completed">Completed</option>
-              <option value="pending">Pending</option>
-            </select>
-          </form>
+        <div class="card mb-4">
+          <div class="card-body">
+            <form>
+              <div class="row g-3 align-items-center">
+                <div class="col-auto">
+                  <label for="todoInput" class="col-form-label">Enter a todo:</label>
+                </div>
+                <div class="col-auto">
+                  <input type="text" value={todoInput} onChange={(e) => setTodoInput(e.target.value)} class="form-control"/>
+                </div>
+                <div class="col-auto">
+                  <button type="submit" onClick={addTodo} class="btn btn-primary">Add</button>
+                </div>
+                <div class="col-auto">
+                  <label for="todoStatus" class="col-form-label">Filter:</label>
+                </div>
+                <div class="col-auto">
+                  <select value={todoStatus} onChange={(e) => setTodoStatus(e.target.value)} class="form-select">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="pending">Pending</option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-        <div>
-          <ul>
-            {todos.map((todo, index) => {
-              // Display all todos
-              if (todoStatus === 'all') {
-                return (
-                <li key={index}>{todo.text} 
-                <button onClick={() => markAsComplete(index)}>Task is {todo.status}.</button>
-                <button onClick={() => editTodo(index)}>Edit</button>
-                <button onClick={() => deleteTodo(index)}>Delete</button>
-                </li>);
-              }
-              // Display only completed todos
-              if (todoStatus === 'completed' && todo.status === 'completed') {
-                return (
-                <li key={index}>{todo.text} 
-                <button onClick={() => markAsComplete(index)}>Task is {todo.status}.</button>
-                <button onClick={() => editTodo(index)}>Edit</button>
-                <button onClick={() => deleteTodo(index)}>Delete</button>
-                </li>);
-              }
-              // Display only pending todos
-              if (todoStatus === 'pending' && todo.status === 'pending') {
-                return (
-                <li key={index}>{todo.text} 
-                <button onClick={() => markAsComplete(index)}>Task is {todo.status}.</button>
-                <button onClick={() => editTodo(index)}>Edit</button>
-                <button onClick={() => deleteTodo(index)}>Delete</button>
-                </li>);
-              }
-            })}
-          </ul>
+        <div class="card">
+          <div class="card-header">
+            <h3>Your Todos</h3>
+          </div>
+          <div class="card-body">
+            <ul class="list-group">
+              {todos.map((todo, index) => {
+                // Display all todos
+                if (todoStatus === 'all') {
+                  return (
+                    <li key={index} class="list-group-item">
+                      <div class="row">
+                        <div class="col-8">
+                          <p>
+                            {todo.text} 
+                          </p>
+                        </div>
+                        <div class="col-auto text-center">
+                          <div className="btn-group" role="group">
+                            <button onClick={() => markAsComplete(index)} className="btn btn-primary">Task is {todo.status}.</button>
+                            <button onClick={() => editTodo(index)} className="btn btn-secondary">Edit</button>
+                            <button onClick={() => deleteTodo(index)} className="btn btn-danger">Delete</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                }
+                // Display only completed todos
+                if (todoStatus === 'completed' && todo.status === 'completed') {
+                  return (
+                    <li key={index} class="list-group-item">
+                      <div class="row">
+                        <div class="col-8">
+                          <p>
+                            {todo.text} 
+                          </p>
+                        </div>
+                        <div class="col-auto text-center">
+                          <div className="btn-group" role="group">
+                            <button onClick={() => markAsComplete(index)} className="btn btn-primary">Task is {todo.status}.</button>
+                            <button onClick={() => editTodo(index)} className="btn btn-secondary">Edit</button>
+                            <button onClick={() => deleteTodo(index)} className="btn btn-danger">Delete</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                }
+                // Display only pending todos
+                if (todoStatus === 'pending' && todo.status === 'pending') {
+                  return (
+                    <li key={index} class="list-group-item">
+                      <div class="row">
+                        <div class="col-8">
+                          <p>
+                            {todo.text} 
+                          </p>
+                        </div>
+                        <div class="col-auto text-center">
+                          <div className="btn-group" role="group">
+                            <button onClick={() => markAsComplete(index)} className="btn btn-primary">Task is {todo.status}.</button>
+                            <button onClick={() => editTodo(index)} className="btn btn-secondary">Edit</button>
+                            <button onClick={() => deleteTodo(index)} className="btn btn-danger">Delete</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          </div>
+          <div class="card-footer">
+            <p>
+              <strong>Total Todos:</strong> {todos.length}
+            </p>
+          </div>
         </div>
-      </header>
+      </div>
     </div>
   );  
 }
